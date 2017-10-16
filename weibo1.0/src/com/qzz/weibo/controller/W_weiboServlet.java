e0b4bba21865724f49285edf6d2b5d5299176418
看看不懂 <825941503@qq.com>
2017-10-16 20:13:55 +0800

    重新提交

--- weibo1.0/src/com/qzz/weibo/controller/W_weiboServlet.java
+++ weibo1.0/src/com/qzz/weibo/controller/W_weiboServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class WeiBoServlet
  */
 @WebServlet("/WeiBoServlet")
-public class W_weiBoServlet extends HttpServlet {
+public class W_weiboServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public W_weiBoServlet() {
+    public W_weiboServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
